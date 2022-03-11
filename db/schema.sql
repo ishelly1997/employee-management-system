0885434f69e65dc8ace1f5b302424d1016c07a03
ishelly1997 <ishelly1997@gmail.com>
2022-03-11 11:37:03 -0500

    seed data, added inquirer package

--- db/schema.sql
+++ db/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE roles(
   REFERENCES departments(id)
 );
 
-CREATE TABLE employee(
+CREATE TABLE employees(
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,26 @@
 /*sample data to add*/
+
+INSERT INTO departments (department_name)
+VALUES ("accounting"),
+       ("human resources"),
+       ("engineering"),
+       ("product development"),
+       ("marketing");
+
+INSERT INTO roles (title, salary)
+VALUES ("Accountant", 60000),
+       ("Engineer", 95000),
+       ("Data Analyst", 120000),
+       ("Human Resources Director", 100000),
+       ("Marketing Specialist", 85000),
+       ("Product Manager",  200000),
+       ("Intern", NULL);
+
+INSERT INTO employees (first_name, last_name)
+VALUES ("Christinith", "Johnson"),
+       ("Harry", "Debby"),
+       ("Carolina", "Stuart"),
+       ("Blerta", "Marku"),
+       ("Ivan", "Milkes"),
+       ("Pepe", "Frenchman"),
+       ("Aiello", "DeCameron");
